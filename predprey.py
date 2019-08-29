085e674080c3dd62fc3fb2471697a733dbbc392a
Jaure020 <46180062+Jaure020@users.noreply.github.com>
2019-08-28 23:50:42 -0700

    Update predprey.py

--- predprey.py
+++ predprey.py
@@ -1,5 +1,5 @@
 # Project Name: Prey and Predator Population Model
-# Date: 06/20/2019
+# Date: 08/28/2019
 # Compiler type: PyCharm
 # File Type: Python 3
 from numpy import *
@@ -35,15 +35,15 @@ predatorDeathRate = 0.7
 # Rate at how efficiently predator reproduce based off the prey
 predatorEfficiencyRate = 1.6'''
 
-# ODE DE equations
+# ODE DE Variables
 
-# the natural growth rate of rabbits
+# the natural growth rate of rabbits(Species 1)
 a = 1
-# death rate of rabbits due to predators
+# death rate of rabbits due to predators(species 2)
 b = 0.1
-# natural death rate of predator when there are no prey
+# natural death rate of predator when there are no prey( natural death rate of species 2 when there is no specie 1)
 c = 1.5
-# the facor describing how many predator eat the prey
+# the facor describing how many predator(Species 2) eat the prey(species 1)
 d = 0.75
 
 
@@ -184,3 +184,98 @@ plt.show()
 # p.title('Wolves and rabbit populations $(1/N_e)$')
 # f3.savefig('rabbits_and_wolves_1.png')
 # plt.show()
+
+#ODE Variable for Mutualism
+# the natural growth rate of (Species 1)
+a = 1
+# death rate of rabbits due to predators(species 2)
+b = 0.1
+# natural death rate of (species 2) when there are no (species 1)
+# the facor describing how many (Species 2) coexist with(species 1)
+d = 0.75
+
+# Calculates growth rate
+def dX_dt(X, t=0):
+   # Returns the growth rate of predator and prey populations
+   return array([a * X[0] +b * X[0] * X[1], c * X[1] + d * b * X[0] * X[1]])
+
+# Population Equilibirum: equilibirum occurs when the growth rate equals 0, this will give two fixed points
+X_f0 = array([0.0, 0.0])
+X_f1 = array([c / (d * b), a / b])
+all(dX_dt(X_f0) == zeros(2)) and all(dX_dt(X_f1) == zeros(2))  # => True
+
+
+# Stability of the fixed points
+# dX_dt = A_f*X where A is the Jacobian matrix evaluated at the corresponding point.
+# We have to define the Jacobian matrix:
+def d2X_dt2(X, t=0):
+   # Return the jacobian matrix evaluated in X
+   return array([[a + b * X[1], b * X[0]], [b * d * X[1], c + b * d * X[0]]])
+
+A_f1 = d2X_dt2(X_f1)
+
+# Eigenvalue are +/- square root (c*a).j:
+lambda1, lambda2 = linalg.eigvals(A_f1)
+
+# They are imaginary numbers so that species 1 and 2 are periodic and the periods are given by
+T_f1 = 2 * pi / abs(lambda1)
+
+# time t = linspace(start,stop,num)
+# Returns num(the third parameter) evenly spaced samples, calculated over the interval [start, stop].
+t = linspace(0, 1000, 1000)
+
+# initial conditions, 10 species 2 and 5 species 1
+X0 = array([10, 5])
+
+X, infodict = integrate.odeint(dX_dt, X0, t, full_output=True)
+infodict['message']
+
+#Linear algebra transposition of 2d-array(matrix)
+species1, species2 = X.T
+
+
+# Creating a list data structure for rabbits and wolves
+ne_r = list(species1)
+ne_w = list(species2)
+
+#function to calculate effective population of rabbit
+def effectivePopSpecies1(X,t):
+   summation = 0
+
+   for i in range(0, t.size):
+       summation += 1 / species1[i]
+       X[i] = float((1 / (t[i] * float(summation))))
+    #returns ne_r
+   return X
+
+
+#function to calculate effective population of wolves
+def effectivePopSpecies2(X,t):
+   summation = 0
+
+   for i in range(0, t.size):
+       summation += 1 / species2[i]
+
+       X[i] = float((1 / (t[i] * float(summation))))
+    #returns ne_w
+   return X
+
+#Fills ne_r and new_w with effective populations for rabbits and wolves
+ne_r = effectivePopSpecies1(ne_r,t)
+ne_w = effectivePopSpecies2(ne_w,t)
+
+
+
+# # Population Scaled Mutation Rates ( 4 NEu) u= specific mutation rates, ne = effectie pop size
+#
+# # Pop scaled mutation rates in predator
+# mutationRate_r=[]
+# mutationRate_w=[]
+# u=0
+#
+# for i in range(0,ne_r.size):
+#     mutationRate_r[i]= (4*(ne_r[i]*u))
+#     mutationRate_w[i] = (4*(ne_r[i]*u))
+#
+# # Pop scaled mutation rates in prey
+# # predatorMutationRatesArray=(4 * (preyEffectiveArray[i] * u)
